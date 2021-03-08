b07af8bd8e6b943b5297bb9d06bf45e679912ff0
Khem chand <khemchand5487@gmail.com>
2021-03-08 19:27:44 +0530

    Update views.py

--- Hemllo_App/views.py
+++ Hemllo_App/views.py
@@ -2,10 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def index(request):
-    # return HttpResponse("<h1>Welcome</h1>")
-    context = {
-        'var':"Just testing!"
-    }
-    return render(request, 'index.html', context=context)
 
