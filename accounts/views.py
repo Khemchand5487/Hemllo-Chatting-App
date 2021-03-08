f887ff2f29ac3ac00c7811068c8c4d74440f8cd2
Khem chand <khemchand5487@gmail.com>
2021-03-08 19:25:48 +0530

    Update views.py

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def login(request):
-    return HttpResponse("Login")
+def index(request):
+    # return HttpResponse("<h1>Welcome</h1>")
+    context = {
+        'var':"Just testing!"
+    }
+    return render(request, 'index.html', context=context)
